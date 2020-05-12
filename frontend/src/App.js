55871bdb8dae05a93402a1d8162aa8e1fcc3a42b
Joseph Cardenas <josephvcardenas@gmail.com>
2020-05-12 09:43:16 -0700

    minor changes to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+ 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">   
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            Upload pdf file below to OCR.
+            </p>
+        {/* TODO: create form  */}   
+        </header>
+        <body className="App-body"> 
+     
+        </body >
+      </div>
+    );
+  }
+  
 }
 
 export default App;
