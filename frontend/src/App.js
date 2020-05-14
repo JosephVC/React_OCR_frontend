14eba8eb1e49047e85eb82674693a5445d194394
Joseph Cardenas <josephvcardenas@gmail.com>
2020-05-14 13:15:38 -0700

    updated App.js to create a more advanced form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,30 +2,35 @@ import React, { Component } from 'react';
  
 import './App.css';
 
-class App extends Component {
+// Create form
+//    This form does more than a standard one, as it logs when a form was submitted
+
+class UploadForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {    
+    this.setState({value: event.target.value}); 
+   }
+  handleSubmit(event) {
+    alert('A file was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">   
-        <p>
-            Upload pdf file below to OCR.
-            </p>
-        {/* TODO: create form  */} 
-        <form>
-          <label>
-            Name:
-            <input type="text" name="name" />  
-          </label>
-          <input type="submit" value="Submit" />  
-        </form>  
-        </header>
-        <body className="App-body"> 
-     
-        </body >
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          File:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />        </label>
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
-  
 }
 
-export default App;
+export default UploadForm;
