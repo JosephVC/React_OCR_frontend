5122d0418fdebbc149f7723429cd8287b904ae32
Joseph Cardenas <josephvcardenas@gmail.com>
2020-05-15 22:48:55 -0700

    the form loads, but django tells me of a bad request to /file/upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,67 @@
-import React, { Component } from 'react';
- 
+import React, {Component} from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 // Create form
 //    This form does more than a standard one, as it logs when a form was submitted
 
-class UploadForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+class UploadForm extends Component {
+  state = {
+    title: '',
+    content:'',
+    file: null
+  };
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  };
 
-  handleChange(event) {    
-    this.setState({value: event.target.value}); 
-   }
-  handleSubmit(event) {
-    alert('A file was submitted: ' + this.state.value);
-    event.preventDefault();
-  }
+  handleFileChange = (e) => {
+    this.setState({
+      file: e.target.files[0]
+    })
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+    let form_data = new FormData();
+    form_data.append('file', this.state.file, this.state.file.name);
+    form_data.append('title', this.state.title);
+    form_data.append('content', this.state.content);
+    let url = 'http://localhost:8000/file/upload/';
+    axios.post(url, form_data, {
+      headers: {
+        'content-type': 'multipart/form-data'
+      }
+    })
+        .then(res => {
+          console.log(res.data);
+        })
+        .catch(err => console.log(err))
+  };
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          File:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <input type="text" placeholder='Title' id='title' value={this.state.title} onChange={this.handleChange} required/>
+          </p>
+          <p>
+            <input type="text" placeholder='Content' id='content' value={this.state.content} onChange={this.handleChange} required/>
+
+          </p>
+          <p>
+            <input type="file"
+                   id="pdf"
+                   accept="image/png, image/jpeg, .pdf"  onChange={this.handleFileChange} required/>
+          </p>
+          <input type="submit"/>
+        </form>
+      </div>
     );
   }
 }
