d7868a606cea4f90eb799bf6a06a51aceb72fcbb
Joseph Cardenas <josephvcardenas@gmail.com>
2020-08-13 15:57:47 -0700

    commit so Russell can look at it

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import UploadForm from './components/UploadForm'
 class App extends Component {
     render() {
         return (
-            <div className="App">
+            <div >
                 <UploadForm />
                 <PdfDisplay />
                 
