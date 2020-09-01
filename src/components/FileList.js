1e43f1859ed354ec6672abdcffff5a18cce479d7
JosephVC <josephvcardenas@gmail.com>
2020-09-01 15:01:54 -0700

    dummy move; now fetch from s3 bucket

--- src/components/FileList.js
+++ src/components/FileList.js
@@ -8,7 +8,10 @@ class FileList extends Component {
     async componentDidMount () {
         try {
             // fetch the data from the api
-            const res = await fetch('http://127.0.0.1:8000/');
+            // local
+            // const res = await fetch('http://127.0.0.1:8000/');
+            // heroku-based backend
+            const res = await fetch('https://ocr-backend-bucket.s3.amazonaws.com/static/post_images/')
             const fileList = await res.json();
             this.setState({
                 fileList
