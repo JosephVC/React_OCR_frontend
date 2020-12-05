5c3b38da6b6350a94818e5cc28ceff69c564f2a2
JosephVC <josephvcardenas@gmail.com>
2020-12-05 10:24:34 -0800

    front end login + logout components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'; 
 import './index.css';
+
+// import components
 import App from './App';
+import Footer from './components/footer';
+import Header from './components/header';
+import Register from './components/register';
+import Login from './components/login';
+import Logout from './components/logout';
+
 // import {DisplayPDF} from './components/pdf-display';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <h1>Online OCR</h1>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const routing = (
+  <Router>
+    <React.StrictMode>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={App} />
+        <Route path='/register' component={Register} />
+        <Route path='/login' component={Login} />
+        <Route path='/logout' component={Logout} />
+      </Switch>
+      <Footer />
+    </React.StrictMode>
+  </Router>
 );
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
