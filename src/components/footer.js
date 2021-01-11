bf487ae04bd04bac797a721e319178bf4d13110c
JosephVC <josephvcardenas@gmail.com>
2021-01-11 13:28:38 -0800

    removed excess material from footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -41,31 +41,8 @@ function Copyright() {
 
 const footers = [
 	{
-		title: 'Company',
-		description: ['Team', 'History', 'Contact us', 'Locations'],
-	},
-	{
-		title: 'Features',
-		description: [
-			'Cool stuff',
-			'Random feature',
-			'Team feature',
-			'Developer stuff',
-			'Another one',
-		],
-	},
-	{
-		title: 'Resources',
-		description: [
-			'Resource',
-			'Resource name',
-			'Another resource',
-			'Final resource',
-		],
-	},
-	{
-		title: 'Legal',
-		description: ['Privacy policy', 'Terms of use'],
+		title: 'Terms of use',
+		description: ['Terms of use'],
 	},
 ];
 
